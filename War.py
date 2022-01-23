00eb1e8c311b92d0bd56c04d6b3fa85911c23a4e
Alexander <alexander@Alexanders-MacBook-Pro.local>
2022-01-22 23:18:54 -0500

    Created classes to make card deck, will make a game of war

---
+++ War.py
@@ -0,0 +1,42 @@
+suits = ('Hearts', 'Diamonds', 'Spades', 'Clubs')
+ranks = ('Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine', 'Ten', 'Jack', 'Queen', 'King', 'Ace')
+values = {'Two':2, 'Three':3, 'Four':4, 'Five':5, 'Six':6, 'Seven':7, 'Eight':8, 
+            'Nine':9, 'Ten':10, 'Jack':11, 'Queen':12, 'King':13, 'Ace':14}
+
+class Card:
+    def __init__(self,suit,rank):
+       self.suit = suit
+       self.rank = rank
+       self.values = values[rank]
+    
+    def __str__(self):
+        return self.rank + ' of ' + self.suit
+
+
+
+class Deck:
+    
+    def __init__(self):
+        # Note this only happens once upon creation of a new Deck
+        self.all_cards = [] 
+        for suit in suits:
+            for rank in ranks:
+                # This assumes the Card class has already been defined!
+                self.all_cards.append(Card(suit,rank))
+                
+    def shuffle(self):
+        # Note this doesn't return anything
+        random.shuffle(self.all_cards)
+        
+    def deal_one(self):
+        # Note we remove one card from the list of all_cards
+        return self.all_cards.pop()        
+    
+    
+four_hearts = Card(suits[0],ranks[2])
+print(four_hearts)
+
+mydeck = Deck()
+
+#Easy Print out list
+print(*mydeck.all_cards, sep="\n")
